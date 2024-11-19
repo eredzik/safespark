ba9acd7f21e0690474c3a65abb2a5fc61aa490de
Emil Redzik <emilredzik1994@gmail.com>
2024-11-19 09:11:58 +0100

    Add generation from spark

--- safespark/generate.py
+++ safespark/generate.py
@@ -1,4 +1,5 @@
 import pyarrow.parquet
+from pyspark.sql import DataFrame
 def generate_from_parquet(parquet_path:str):
     schema=pyarrow.parquet.read_schema(parquet_path, memory_map=True)
     names =', '.join([f'"{name}"' for name in schema.names])
@@ -6,3 +7,7 @@ def generate_from_parquet(parquet_path:str):
     return declaration
     
 
+def generate_from_spark(spark_df:DataFrame):
+    names =', '.join([f'"{name}"' for name in spark_df.columns])
+    declaration = f"Dataset[Literal[{names}]]"
+    return declaration
